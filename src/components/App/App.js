a704408b8fdd7a6b528893c34ed6ee53d015fe11
Vitaly <vitalgolob@gmail.com>
2020-09-04 15:10:42 +0300

    add Button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 import Todo from "../Todo/Todo";
 
+const data = [
+  { id: 1, title: "to read" },
+  { id: 2, title: "to to write" },
+  { id: 3, title: "to sleep" },
+];
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <Todo />
+      <div className="container">
+        <h1>To-Do LIST</h1>
+        {data.map((todo) => (
+          <Todo key={todo.id} title={todo.title} />
+        ))}
       </div>
     );
   }
--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
+import "./Todo.css";
 
-const Todo = () => {
-  return (
-    <div>
-      <h1>ToDo</h1>
-    </div>
-  );
-};
+class Todo extends Component {
+  delete = (e, title) => console.log("DEL", title);
+  render() {
+    const { title } = this.props;
+    return (
+      <div>
+        <p>{title}</p>
+        <button
+          onClick={(e) => this.delete(e, title)}
+          className="btn btn-danger"
+        >
+          DEL
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Todo;
