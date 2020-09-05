b566930b7f1e1b639e0446a67dc5122630c453f0
Vitaly <vitalgolob@gmail.com>
2020-09-05 12:46:58 +0300

    add State

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,42 @@
 import React, { Component } from "react";
 import Todo from "../Todo/Todo";
 
-const data = [
-  { id: 1, title: "to read" },
-  { id: 2, title: "to to write" },
-  { id: 3, title: "to sleep" },
-];
+// const data = [
+//   { id: 1, title: "to read" },
+//   { id: 2, title: "to to write" },
+//   { id: 3, title: "to sleep" },
+// ];
 
 class App extends Component {
+  state = {
+    todos: [
+      { id: 1, title: "to read" },
+      { id: 2, title: "to to write" },
+      { id: 3, title: "to sleep" },
+    ],
+  };
+  deleteTodo = (e, id) => {
+    this.setState({ todos: this.state.todos.filter((todo) => todo.id !== id) });
+  };
   render() {
+    if (this.state.todos.length === 0) {
+      return <h1 style={{ textAlign: "center" }}>No Tasks</h1>;
+    }
+
     return (
+      // console.log('-- ',todos);
       <div className="container">
         <h1>To-Do LIST</h1>
-        {data.map((todo) => (
-          <Todo key={todo.id} title={todo.title} />
-        ))}
+        {this.state.todos.map((todo) => {
+          return (
+            <Todo
+              key={todo.id}
+              id={todo.id}
+              title={todo.title}
+              deleteTodo={this.deleteTodo}
+            />
+          );
+        })}
       </div>
     );
   }
--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -2,16 +2,14 @@ import React, { Component } from "react";
 import "./Todo.css";
 
 class Todo extends Component {
-  delete = (e, title) => console.log("DEL", title);
+  // delete = (e, title) => console.log("DEL", title);
   render() {
-    const { title } = this.props;
+    const { id, title, deleteTodo } = this.props;
     return (
       <div>
         <p>{title}</p>
-        <button
-          onClick={(e) => this.delete(e, title)}
-          className="btn btn-danger"
-        >
+        {/* <p>{count}</p> */}
+        <button onClick={(e) => deleteTodo(e, id)} className="btn btn-danger">
           DEL
         </button>
       </div>
